51c56edcaa73a988ffd43a61d72109d28ff073d0
Parth Maheshwari <155217469+Parthmaheshwari2410@users.noreply.github.com>
2025-09-18 15:54:13 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -12,7 +12,7 @@ import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
 
-export const url = "https://relief-backend-5af3.onrender.com"
+export const url = "https://relief-backend-whgf.onrender.com"
 
 export const assets = {
     add_icon,
